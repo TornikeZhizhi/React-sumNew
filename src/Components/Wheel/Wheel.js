77f2c24173800f6983efb314df4ddda38e61898e
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2022-04-30 00:43:18 +0400

    add wheel cmpt

--- src/Components/Wheel/Wheel.js
+++ src/Components/Wheel/Wheel.js
@@ -1,87 +1,126 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./css/wheel.css";
 import WheelItem from './WheelItem';
-
-
-
-
-  //   wheel js
-
-//   $(".wheel__button").click(function() {
-//     $(this).addClass('inactive');
-// });
-
-//         var spinValue = 0;
-//         var initialSpinRotation = 0;
-//         var sectionAmount = 16;
-//         var spinCount = 5;
-//         var initialRotationDeg = 0;
-//         var winningNumber = 0;
-//         var wheelOffset = 0;
-//         var freebetsWon = 0;
-
-//         function updateAnimationNumbers(prizeSection) {
-//             spinValue = (360 * spinCount) - (prizeSection * 360 / sectionAmount) + initialRotationDeg - wheelOffset;
-
-//             console.log(winningNumber,"winningNumber");
-
-//             const CSSTemplate = `
-//                 @keyframes spinning {
-//                 from { transform: rotate(${initialSpinRotation}deg); }
-//                 to {  transform: rotate(${initialSpinRotation + spinValue}deg); }
-//                 }
-//             `;
-//             $('head').append('<style id="keyframes">'+CSSTemplate+'</style>');
-//         }
-
-//         $(".wheel__button").click(function(){
-
-//             $(".wheel").addClass("anime")
-//             // winningNumber = prizeTranslations[0].PRIZE_ID];
-
-//             winningNumber = Math.floor(Math.random() * 16);
-        
-//             setTimeout(() => {
-//                 updateAnimationNumbers(winningNumber);
-//                 $(".wheel__white").addClass('spinning');
-                
-//                 $(".spinning").one("animationend oAnimationEnd webkitAnimationEnd",
-//                     function(event) {
-//                     $(".wheel__white").removeClass("spinning");
-//                     $(".wheel__element").removeClass("active");
-//                     $(".wheel__button").removeClass("inactive");
-                        
-//                     $(".wheel__arrow").addClass("up")
-//                     setTimeout(function(){
-//                         $(".wheel-winner").fadeIn(250);
-
-//                     },2500)
-                    
-//                     initialSpinRotation += spinValue;
-//                     wheelOffset = initialSpinRotation % 360;
-//                     // $(".wheel").removeClass("anime")
-//                     // console.log(wheelOffset,"wheelOffset");
-//                     // console.log(initialSpinRotation,"initialSpinRotation")
-//                     $(".wheel__element").eq(winningNumber).addClass('active');
-//                     $(".wheel__white").css({ 'WebkitTransform': 'rotate(' + initialSpinRotation + 'deg)'});
-//                 });
-//             }, 1000);
-//         });
-
-//         $(".wheel-winner__close, .wheel-winner__button").click(function(){
-//             $(".wheel-winner").fadeOut(200);
-//             $(".wheel").removeClass('anime');
-//             $(".wheel__element").removeClass('active');
-//             $(".wheel__arrow").removeClass("up")
-//           })
-    
-
-
-
+import Dummy_WheelArray from "./WheelData.json";
 
 const Wheel = () => {
+
+
+    const [wheelArray, setWheelArray] = useState(Dummy_WheelArray);
+    const [wheelAnime, setWheelAnime] = useState(false)
+    const [initialSpinRotation, setInitialSpinRotation] = useState(0)
+    const [wheelOffset, setwheelOffset] = useState(0);
+  
+    let spinValue = 0;
+    let sectionAmount = 16;
+    let spinCount = 5;
+    let winningNumber = 1;
+    
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            wheelArray.map(item=>{
+                if(item.activeClass == true) {
+                    console.log("shevida");
+                     removeActiveClass()
+                }
+            })
+        },3300)
+    },[wheelArray])
+
+    function updateAnimationNumbers(prizeSection) {
+        spinValue = (360 * spinCount) - (prizeSection * 360 / sectionAmount) - wheelOffset;
+        
+    
+        const CSSTemplate = `
+            @keyframes spinning {
+            from { transform: rotate(${initialSpinRotation}deg); }
+            to {  transform: rotate(${initialSpinRotation + spinValue}deg); }
+            }
+        `;
+        const $style = document.createElement("style");
+        document.head.appendChild($style);
+        $style.innerHTML = CSSTemplate;
+       
+    }
+
+
+    const addActiveClass = () => {
+        const updateArray = wheelArray.map(item=>{
+            if(item.id == winningNumber){
+                console.log(item.id, winningNumber)
+                return {...item,activeClass:true}
+            }else {
+                return item
+            }
+        })
+        setWheelArray(updateArray)
+    }
+
+    const removeActiveClass = () =>{
+        const updateArray2 = wheelArray.map(item=>{
+            return {...item,activeClass:false}
+    })
+         setWheelArray(updateArray2)
+    }
+
+    const startHandler = () => {
+     
+
+        winningNumber = Math.floor(Math.random() * 16 + 1)
+
+        updateAnimationNumbers(winningNumber)
+        setWheelAnime(true);
+
+        document.getElementById("spinning").addEventListener("animationend", function() {
+            
+       
+            let SynSpinRotation;
+            setInitialSpinRotation((prev)=>{
+                 return prev + spinValue
+            })
+            SynSpinRotation = initialSpinRotation + spinValue
+            setWheelAnime(false)
+            setwheelOffset(SynSpinRotation % 360)
+            
+            addActiveClass()
+           
+
+        }, {once : true})
+
+    }
+
+
     return (
-        <WheelItem />
+        <div className="bonus-game">
+         {initialSpinRotation}
+        <div className="bonus-game__wrapper">
+          <div className="bonus-game__wheel">
+            <div className={`wheel ${wheelAnime ? 'anime' : null}`}>
+              <div className={`wheel__button ${wheelAnime ? 'inactive':null}`} onClick={startHandler}></div>
+              <div className="wheel__arrow"></div>
+              <div id='spinning' className={`wheel__white ${wheelAnime ? 'spinning' : null}`}
+                style={{transform:`rotate(${initialSpinRotation}deg)`}}>
+                <div className="wheel__mid">
+                  <div className="wheel__inner">
+                  
+
+                    {wheelArray.map(item=>
+                      <WheelItem 
+                        textBlack={item.textBlack}
+                        iconClass={item.iconClass}
+                        activeClass={item.activeClass}
+                        text={item.text}
+                        key={item.id}/>
+                        )}
+                    <div className="wheel__shadow"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
 };
 
--- src/Components/Wheel/WheelItem.js
+++ src/Components/Wheel/WheelItem.js
@@ -1,153 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-
-
-const WheelItem = () => {
-
-    const Dummy_WheelArray = [
-        {text:"1000₾",id:0, iconClass:"gel", textBlack:false,activeClass:false},
-        {text:"5₾",id:1, iconClass:"p2p", textBlack:false,activeClass:false},
-        {text:"10₾",id:2, iconClass:"dice", textBlack:true,activeClass:false},
-        {text:"100₾",id:3, iconClass:"p2p", textBlack:false,activeClass:false},
-        {text:"20₾",id:4, iconClass:"dice", textBlack:true,activeClass:false},
-        {text:"10000₾",id:5, iconClass:"gel", textBlack:false,activeClass:false},
-        {text:"50₾",id:6, iconClass:"dice", textBlack:true,activeClass:false},
-        {text:"20₾",id:7, iconClass:"gel", textBlack:false,activeClass:false},
-        {text:"1₾",id:8, iconClass:"dice", textBlack:true,activeClass:false},
-        {text:"20₾",id:9, iconClass:"p2p", textBlack:false,activeClass:false},
-        {text:"250₾",id:10, iconClass:"gel", textBlack:false,activeClass:false},
-        {text:"300₾",id:11, iconClass:"p2p", textBlack:false,activeClass:false},
-        {text:"50₾",id:12, iconClass:"dice", textBlack:true,activeClass:false},
-        {text:"1500₾",id:13, iconClass:"p2p", textBlack:false,activeClass:false},
-        {text:"1700₾",id:14, iconClass:"dice", textBlack:true,activeClass:false},
-        {text:"1000₾",id:15, iconClass:"p2p", textBlack:false,activeClass:false},
-    ]
-    
-    const [wheelArray, setWheelArray] = useState(Dummy_WheelArray);
-    const [wheelAnime, setWheelAnime] = useState(false)
-    const [initialSpinRotation, setInitialSpinRotation] = useState(0)
-    const [wheelOffset, setwheelOffset] = useState(0);
-    // const [winIndex, setWinIndex] = useState(null)
-
-    // console.log(wheelArray)
-    let spinValue = 0;
-    let sectionAmount = 16;
-    let spinCount = 5;
-    let winningNumber = 1;
-    
-
-    useEffect(()=>{
-        setTimeout(()=>{
-            wheelArray.map(item=>{
-                if(item.activeClass == true) {
-                    console.log("shevida");
-                     removeActiveClass()
-                }
-            })
-        },3300)
-    },[wheelArray])
-
-    function updateAnimationNumbers(prizeSection) {
-        spinValue = (360 * spinCount) - (prizeSection * 360 / sectionAmount) - wheelOffset;
-        
-    
-        const CSSTemplate = `
-            @keyframes spinning {
-            from { transform: rotate(${initialSpinRotation}deg); }
-            to {  transform: rotate(${initialSpinRotation + spinValue}deg); }
-            }
-        `;
-        const $style = document.createElement("style");
-        document.head.appendChild($style);
-        $style.innerHTML = CSSTemplate;
-       
-    }
-
-
-    const addActiveClass = () => {
-        const updateArray = wheelArray.map(item=>{
-            if(item.id == winningNumber){
-                console.log(item.id, winningNumber)
-                return {...item,activeClass:true}
-            }else {
-                return item
-            }
-        })
-        setWheelArray(updateArray)
-    }
-
-    const removeActiveClass = () =>{
-        const updateArray2 = wheelArray.map(item=>{
-            return {...item,activeClass:false}
-    })
-         setWheelArray(updateArray2)
-    }
-
-    const startHandler = () => {
-     
-
-        winningNumber = Math.floor(Math.random() * 16 + 1)
-
-        updateAnimationNumbers(winningNumber)
-        setWheelAnime(true);
-
-        document.getElementById("spinning").addEventListener("animationend", function() {
-            
-       
-            let SynSpinRotation;
-            setInitialSpinRotation((prev)=>{
-                 return prev + spinValue
-            })
-            SynSpinRotation = initialSpinRotation + spinValue
-            setWheelAnime(false)
-            setwheelOffset(SynSpinRotation % 360)
-            
-            addActiveClass()
-           
-
-        }, {once : true})
-
-    }
-
-
+const WheelItem = ({textBlack,iconClass,activeClass,text}) => {
     return (
-        <div className="bonus-game">
-         {initialSpinRotation}
-        <div className="bonus-game__wrapper">
-          <div className="bonus-game__wheel">
-            <div className={`wheel ${wheelAnime ? 'anime' : null}`}>
-              <div className={`wheel__button ${wheelAnime ? 'inactive':null}`} onClick={startHandler}></div>
-              <div className="wheel__arrow"></div>
-              <div id='spinning' className={`wheel__white ${wheelAnime ? 'spinning' : null}`}
-                style={{transform:`rotate(${initialSpinRotation}deg)`}}>
-                <div className="wheel__mid">
-                  <div className="wheel__inner">
-                  
-
-                    {wheelArray.map(item=>
-                        <div className={`wheel__element ${item.activeClass?"active":null}`} key={item.id}>
-                        <div className="wheel__element-anker">
-                        <div className="wheel__element-white"></div>
-                        <div className="wheel__element-inactive"></div>
-                        <div className="wheel__element-active">
-                            <div className="wheel__element-fire"></div>
-                        </div>
-                        <div className="wheel__element-content">
-                            <div className={`wheel__element-text ${item.textBlack ? "black":null}`}> {item.text}</div>
-                            <div className={`wheel__element-icon ${item.iconClass}`}></div>
-                        </div>
-                        </div>
-                        </div>
-                        
-                        )}
-                    <div className="wheel__shadow"></div>
-                  </div>
-                </div>
-              </div>
+        <>
+        <div className={`wheel__element ${activeClass?"active":null}`}>
+            <div className="wheel__element-anker">
+            <div className="wheel__element-white"></div>
+            <div className="wheel__element-inactive"></div>
+            <div className="wheel__element-active">
+                <div className="wheel__element-fire"></div>
             </div>
-          </div>
+            <div className="wheel__element-content">
+                <div className={`wheel__element-text ${textBlack ? "black":null}`}> {text}</div>
+                <div className={`wheel__element-icon ${iconClass}`}></div>
+            </div>
+            </div> 
         </div>
-      </div>
+        </>
     );
 };
 
