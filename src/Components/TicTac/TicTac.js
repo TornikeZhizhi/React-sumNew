79ef2a1664c98557b8fa9224a7a143267f4d70e8
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2023-11-07 03:57:52 +0400

    add indexis

--- src/Components/TicTac/TicTac.js
+++ src/Components/TicTac/TicTac.js
@@ -15,6 +15,28 @@ const userDataList = [
     birth_day: "8/20/2004",
   },
   {
+    full_name: "Colet Briamo",
+    status: "active",
+    points: 90,
+    gender: "Male",
+    mail: "cbriamo0@ucoz.ru",
+    mobile_numbers: "595 30 40 59",
+    id_number: 11784188,
+    address: "7 Banding Court",
+    birth_day: "8/20/2004",
+  },
+  {
+    full_name: "Colet Briamo",
+    status: "active",
+    points: 90,
+    gender: "Male",
+    mail: "cbriamo0@ucoz.ru",
+    mobile_numbers: "595 30 40 59",
+    id_number: 11784188,
+    address: "7 Banding Court",
+    birth_day: "8/20/2004",
+  },
+  {
     full_name: "Henka Castanaga",
     status: "inactive",
     points: 59,
@@ -107,7 +129,30 @@ const TicTac = ({ data }) => {
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
-
+  const nextHandler = () => {
+    setCurrentPage((prev) => {
+      if (totalPages > prev) {
+        return prev + 1;
+      } else {
+        return totalPages;
+      }
+    });
+  };
+  const prevHandler = () => {
+    setCurrentPage((prev) => {
+      if (prev > 1) {
+        return prev - 1;
+      } else {
+        return 1;
+      }
+    });
+  };
+  const lastPageHandler = () => {
+    setCurrentPage(totalPages);
+  };
+  const firstPageHandler = () => {
+    setCurrentPage(1);
+  };
   useEffect(() => {
     setCurrentPage(1); // Reset current page when data or checkboxes change
   }, []);
@@ -143,6 +188,8 @@ const TicTac = ({ data }) => {
 
       {/* Pagination buttons */}
       <div>
+        <div onClick={firstPageHandler}>first page</div>
+        <div onClick={prevHandler}>prev</div>
         {Array.from({ length: totalPages }, (_, index) => index + 1).map(
           (page) => (
             <button key={page} onClick={() => handlePageChange(page)}>
@@ -150,6 +197,8 @@ const TicTac = ({ data }) => {
             </button>
           )
         )}
+        <div onClick={nextHandler}>next</div>
+        <div onClick={lastPageHandler}>last page</div>
       </div>
     </div>
   );
